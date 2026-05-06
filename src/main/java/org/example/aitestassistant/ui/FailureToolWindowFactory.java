21274687f1b33df3535c5664b3579ea3360bf33d
Win-10 <azimovanvarjon777@gmail.com>
2026-05-06 21:10:03 +0200

    feat: add test failure listener with async AI analysis

--- src/main/java/org/example/aitestassistant/ui/FailureToolWindowFactory.java
+++ src/main/java/org/example/aitestassistant/ui/FailureToolWindowFactory.java
@@ -1,2 +1,24 @@
-package org.example.aitestassistant.ui;public class FailureToolWindowFactory {
+package org.example.aitestassistant.ui;
+
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.wm.ToolWindow;
+import com.intellij.openapi.wm.ToolWindowFactory;
+import com.intellij.ui.content.Content;
+import com.intellij.ui.content.ContentFactory;
+import org.jetbrains.annotations.NotNull;
+
+public class FailureToolWindowFactory implements ToolWindowFactory {
+
+
+    public static FailureToolWindowPanel panel;
+
+    @Override
+    public void createToolWindowContent(@NotNull Project project, @NotNull ToolWindow toolWindow) {
+
+        panel = new FailureToolWindowPanel();
+
+        ContentFactory contentFactory = ContentFactory.getInstance();
+        Content content = contentFactory.createContent(panel.getContent(), "", false);
+        toolWindow.getContentManager().addContent(content);
+    }
 }
